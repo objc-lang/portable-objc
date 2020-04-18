834b1895d53f03d8e4a52597a9685a6523485919
stes <stes@telenet.be>
2020-04-18 21:23:17 +0200

    Update to 3.3.19

--- src/objc/objc1.m
+++ src/objc/objc1.m
@@ -1,6 +1,6 @@
 
 /* 
- * Copyright (c) 1998-2019 David Stes
+ * Copyright (c) 1998-2020 David Stes
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Library General Public License as published 
@@ -43,7 +43,7 @@ void printversion(void)
 
 void printcopyright(void)
 {
-  printf("Portable Object Compiler %s (c) 1997-2019.\n",OBJCVERSION);
+  printf("Portable Object Compiler %s (c) 1997-2020.\n",OBJCVERSION);
   printf("Distributed under the terms of the GNU LGPL.\n");
 }
 
